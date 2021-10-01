ee2a8e40ffa92f4b62d8163ab973010e105d4efc
DAHMANE Sara <dahmanesarah@outlook.fr>
2021-10-01 10:48:33 +0200

    TP3 JS

---
+++ JS/JS-TP/TP-3.js
@@ -0,0 +1,13 @@
+const TonAge = prompt("saisir votre age : ");
+
+if (TonAge >= 7 && TonAge <= 9) {
+  alert("poussin");
+} else if (TonAge >= 10 && TonAge <= 11) {
+  alert("pupille");
+} else if (TonAge >= 12 && TonAge <= 13) {
+  alert("benjamin");
+} else if (TonAge >= 14 && TonAge <= 15) {
+  alert("Minime");
+} else if (TonAge >= 16 && TonAge <= 17) {
+  alert("Cadet");
+}
